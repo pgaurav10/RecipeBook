ed591c1e80181594900f8be7bff2b0d12b2beb24
Gaurav <gauravpatil24121997@gmail.com>
2024-02-05 17:27:10 -0800

    Initial Commit

---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Gaurav Patil on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
